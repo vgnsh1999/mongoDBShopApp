a50d6108d3e380b673c178634b793777425e0098
vgnsh1999 <vigneshvarathan@gmail.com>
2023-08-09 15:35:45 +0530

    Relations between Tables, add to cart by user

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("64d358619a95599236b320d4")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name,user.email,user.cart,user._id);
       next();
     })
     .catch(err => console.log(err));
