bc19b0b5a2b5785cb48c3b10a893ca711aed773b
vgnsh1999 <vigneshvarathan@gmail.com>
2023-08-09 10:26:57 +0530

    Connecting shop app with mongoDB

---
+++ util/database.js
@@ -0,0 +1,31 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://vigneshvaradhank:vigneshvaradhank123@cluster0.b2seyrm.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db()
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () =>{
+  if(_db){
+    return _db;
+  }
+  throw 'No database found';
+}
+
+//module.exports = mongoConnect;
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
