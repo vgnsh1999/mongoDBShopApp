869bc7cf00c02eba6fe36863221f1df19fb53333
vgnsh1999 <vigneshvarathan@gmail.com>
2023-08-10 09:03:47 +0530

    Getting Orders Placed

--- models/user.js
+++ models/user.js
@@ -65,6 +65,35 @@ class User{
       {$set:{cart:{items: updatedCartItems}}});
   }
 
+  addOrder(){
+    const db = getDb();
+    return this.getCart().then(products=>{
+      const order = {
+        items:products,
+        users:{
+          _id: new ObjectId(this._id),
+          name:this.name,
+        }
+      };
+    return db.collection('orders').insertOne(order)
+    })
+    .then(result=>{
+      this.cart = {items:[]};
+      return db.collection('users')
+      .updateOne(
+        {_id: new ObjectId(this._id)},
+        {$set:{cart:{items: []}}});
+    });
+  }
+
+  getOrders(){
+    const db = getDb();
+    return db
+    .collection('orders')
+    .find({'user._id':new ObjectId(this._id)})
+    .toArray();
+  }
+
   static findById(userId){
     const db = getDb();
     return db.collection('users').findOne({_id: new ObjectId(userId)})
